836a57b487fd5517c4536667699fa70607d984e3
Kevin Mamani <kevin72309@gmail.com>
2024-09-06 23:02:11 -0300

    fix: ts aliases warnings solved

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig, AliasOptions } from "vite";
 import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -9,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
-    },
+    } as AliasOptions,
   },
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
 });
