9d878656692ca702e6b6ccda151a696939ffa761
Kevin Mamani <kevin72309@gmail.com>
2024-09-08 19:13:23 -0300

    initial language to spanish

--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -1,7 +1,7 @@
-import { initReactI18next } from 'react-i18next';
-import i18n from 'i18next';
-import enLang from './locales/en.json';
-import esLang from './locales/es.json';
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import enLang from "./locales/en.json";
+import esLang from "./locales/es.json";
 
 const resources = {
   en: { translation: enLang },
@@ -9,15 +9,13 @@ const resources = {
 };
 
 // * Configuration
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: 'en',
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "es",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
