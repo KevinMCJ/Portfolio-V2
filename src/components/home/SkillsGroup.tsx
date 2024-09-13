a9bf00549e9f7b114a9c2b16762d82eb8f5affbd
Kevin Mamani <kevin72309@gmail.com>
2024-09-12 21:57:07 -0300

    feat: skills section almost finished

---
+++ src/components/home/SkillsGroup.tsx
@@ -0,0 +1,48 @@
+import i18n from "@/utils/i18n";
+import { useTranslation } from "react-i18next";
+
+interface Technology {
+  name: string;
+  icon: string;
+}
+
+type LanguageKeys = keyof typeof i18n.languages;
+
+interface SkillGroup {
+  category: Record<LanguageKeys, string> | string;
+  technologies: Technology[];
+}
+
+interface SkillsGroupProps {
+  skills: SkillGroup;
+}
+
+const SkillsGroup: React.FC<SkillsGroupProps> = ({ skills }) => {
+  const { i18n } = useTranslation();
+
+  const categoryName =
+    typeof skills.category === "string"
+      ? skills.category
+      : skills.category[i18n.language as LanguageKeys];
+
+  return (
+    <div className="dark:via-primary-400/4 h-[400px] w-full overflow-hidden rounded-md bg-gradient-to-br from-secondary-200 via-secondary-100 to-secondary-300/50 p-4 shadow-[-18px_15px_20px_-18px] shadow-primary-600 dark:from-primary-700/80 dark:via-primary-800/80 dark:to-primary-900/60 dark:shadow-primary-800">
+      <h3 className="mb-5 bg-gradient-to-r from-secondary-900 to-secondary-400 bg-clip-text text-center text-2xl font-bold uppercase text-transparent dark:from-primary-900 dark:via-primary-200 dark:to-primary-800">
+        {categoryName}
+      </h3>
+      <div className="flex flex-wrap place-content-center gap-14 gap-y-10">
+        {skills.technologies.map((tech) => (
+          <div
+            key={tech.name}
+            className="vstack items-center justify-center gap-1"
+          >
+            <img src={tech.icon} alt={tech.name} className="size-12" />
+            <span className="font-semibold uppercase">{tech.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SkillsGroup;
--- src/components/home/SkillsSection.tsx
+++ src/components/home/SkillsSection.tsx
@@ -1,7 +1,18 @@
+import { useTranslation } from "react-i18next";
+import data from "@/utils/skills.json";
+import SkillsGroup from "./SkillsGroup";
+
 const SkillsSection = () => {
+  const { t } = useTranslation();
+
   return (
-    <section id="skills" className="app-container h-[800px]">
-      SkillsSection
+    <section id="skills" className="lg:section-max-h app-container lg:h-screen">
+      <h2 className="section-title">{t("skills-title")}</h2>
+      <div className="grid items-center justify-items-center gap-5 shadow-active md:grid-cols-2 lg:px-5">
+        {data.skills.map((skills, index) => (
+          <SkillsGroup key={index} skills={skills} />
+        ))}
+      </div>
     </section>
   );
 };
