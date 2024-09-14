e58d991f92e80cf11779283909778c2af0b64318
Kevin Mamani <kevin72309@gmail.com>
2024-09-14 11:12:17 -0300

    more icons / adjust theme button mobile size

--- src/components/common/ThemeButton.tsx
+++ src/components/common/ThemeButton.tsx
@@ -1,39 +1,39 @@
-import { useEffect, useState } from 'react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const ThemeButton = () => {
   const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
-    const isDarkTheme = localStorage.getItem('theme') === 'dark';
-    const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const shouldUseDarkTheme = isDarkTheme || (!localStorage.getItem('theme') && isSystemDark);
+    const isDarkTheme = localStorage.getItem("theme") === "dark";
+    const isSystemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const shouldUseDarkTheme = isDarkTheme || (!localStorage.getItem("theme") && isSystemDark);
 
     if (shouldUseDarkTheme) {
-      document.documentElement.classList.add('dark');
+      document.documentElement.classList.add("dark");
       setIsDark(true);
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
   }, []);
 
   const toggleTheme = () => {
     const newIsDark = !isDark;
     setIsDark(newIsDark);
-    document.documentElement.classList.toggle('dark');
-    localStorage.setItem('theme', newIsDark ? 'dark' : 'light');
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", newIsDark ? "dark" : "light");
   };
 
   return (
     <button
       onClick={toggleTheme}
-      className="fixed center bottom-4 right-4 size-14 rounded-full bg-secondary-600 text-white transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
-      aria-label={isDark ? 'Switch to light mode' : 'Switch to dark mode'}
+      className="focus:ring-primary center fixed bottom-4 right-4 size-12 rounded-full bg-secondary-600 text-white transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 md:size-14"
+      aria-label={isDark ? "Switch to light mode" : "Switch to dark mode"}
     >
       {isDark ? (
-        <FaSun className="size-6" aria-hidden="true" />
+        <FaSun className="size-[50%]" aria-hidden="true" />
       ) : (
-        <FaMoon className="size-6" aria-hidden="true" />
+        <FaMoon className="size-[50%]" aria-hidden="true" />
       )}
     </button>
   );
