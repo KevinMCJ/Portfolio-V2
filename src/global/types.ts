b78c2b36cac48d8268c3d4079d28e29d6e14bfb1
Kevin Mamani <kevin72309@gmail.com>
2024-11-13 15:32:07 -0300

    new project atrributes + typing

--- src/global/types.ts
+++ src/global/types.ts
@@ -1,9 +1,12 @@
 export interface Project {
   title: { [key: string]: string };
+  unix_timestamp: { from: number; to: number | undefined };
   description: { [key: string]: string };
-  htmlDescription: { [key: string]: string };
+  html_description: { [key: string]: string };
   thumbnail: string;
   images: string[];
   external_link: { type: string; url: string };
+  video: { exists: boolean; url: string };
+  tags: { [key: string]: string };
   technologies: string[];
 }
