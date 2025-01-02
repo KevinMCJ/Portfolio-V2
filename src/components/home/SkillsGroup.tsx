1ce135321db14706a671c4f6be2ba10e118bf07e
Kevin Mamani <kevin72309@gmail.com>
2025-01-02 17:12:37 -0300

    style: skills section - responsive fix

--- src/components/home/SkillsGroup.tsx
+++ src/components/home/SkillsGroup.tsx
@@ -26,11 +26,11 @@ const SkillsGroup: React.FC<SkillsGroupProps> = ({ skills }) => {
       : skills.category[i18n.language as LanguageKeys];
 
   return (
-    <div className="dark:via-primary-400/4 h-[400px] w-full overflow-hidden rounded-md bg-gradient-to-br from-secondary-200 via-secondary-100 to-secondary-300/50 p-4 shadow-[-18px_15px_20px_-18px] shadow-primary-600 dark:from-primary-700/80 dark:via-primary-800/80 dark:to-primary-900/60 dark:shadow-primary-800">
+    <div className="dark:via-primary-400/4 w-full overflow-hidden rounded-md bg-gradient-to-br from-secondary-200 via-secondary-100 to-secondary-300/50 p-4 shadow-[-18px_15px_20px_-18px] shadow-primary-600 dark:from-primary-700/80 dark:via-primary-800/80 dark:to-primary-900/60 dark:shadow-primary-800 md:h-[400px]">
       <h3 className="mb-5 bg-gradient-to-r from-secondary-900 to-secondary-400 bg-clip-text text-center text-2xl font-bold uppercase text-transparent dark:from-primary-900 dark:via-primary-200 dark:to-primary-800">
         {categoryName}
       </h3>
-      <div className="flex flex-wrap place-content-center gap-14 gap-y-10">
+      <div className="flex flex-wrap place-content-center gap-10 md:gap-14 md:gap-y-10">
         {skills.technologies.map((tech) => (
           <div
             key={tech.name}
--- src/components/home/SkillsSection.tsx
+++ src/components/home/SkillsSection.tsx
@@ -6,7 +6,7 @@ const SkillsSection = () => {
   const { t } = useTranslation();
 
   return (
-    <section id="skills" className="lg:section-max-h app-container lg:h-screen">
+    <section id="skills" className="lg:section-max-h app-container">
       <h2 className="section-title">{t("skills-title")}</h2>
       <div className="grid items-center justify-items-center gap-5 shadow-active md:grid-cols-2">
         {data.skills.map((skills, index) => (
