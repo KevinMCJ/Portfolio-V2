49b6c2c62492f37788e6215e73fe22d02ae096e6
Kevin Mamani <kevin72309@gmail.com>
2025-02-27 16:52:43 -0300

    style: adjust timeline

--- src/components/home/ExperienceSection.tsx
+++ src/components/home/ExperienceSection.tsx
@@ -9,7 +9,7 @@ const ExperienceSection = () => {
     <section id="experience" className="app-container">
       <h2 className="section-title">{t("experience-title")}</h2>
       <div className="center relative mx-auto flex-col gap-12 md:w-[90%]">
-        <div className="absolute left-1/2 z-0 h-full w-1 -translate-x-1/2 transform bg-primary-300" />
+        <div className="absolute left-1/2 top-6 z-0 h-full w-1 -translate-x-1/2 transform rounded-md bg-secondary-400 dark:bg-primary-400" />
         {data.items.map((item, index) => (
           <TimelineEvent item={item} key={index} isEven={index % 2 == 0} />
         ))}
