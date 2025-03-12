4b1e776ae85a8e819f96e18854095300ea642bec
Kevin Mamani <kevin72309@gmail.com>
2025-03-12 13:14:58 -0300

    feat(contact): copy email to clipboard and form implementation

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -1,7 +1,29 @@
+import { useTranslation } from "react-i18next";
+import ContactForm from "./ContactForm";
+import data from "@/utils/data.json";
+
 const ContactSection = () => {
+  const { t } = useTranslation();
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data.email);
+  };
+
   return (
-    <section id="contact" className="container">
-      ContactSection
+    <section id="contact" className="app-container">
+      <h2 className="section-title">{t("contact-title")}</h2>
+      <span
+        className="mx-auto block max-w-fit cursor-pointer p-2 text-xl tracking-wide underline decoration-icon underline-offset-4 sm:text-2xl"
+        onClick={handleCopy}
+        aria-label={`Email: ${data.email}`}
+        title={t("copy-message")}
+      >
+        {data.email}
+      </span>
+      <p className="text-center text-sm tracking-wide text-secondary-900/80 dark:text-secondary-200/95 sm:text-base">
+        {t("contact-message")}
+      </p>
+      <ContactForm />
     </section>
   );
 };
