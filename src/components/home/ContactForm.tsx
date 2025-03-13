b99549c8acd5dd0c1d24f85e7ead324726828ec2
Kevin Mamani <kevin72309@gmail.com>
2025-03-13 18:47:18 -0300

    feat(contact): form validation + ui

--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { IoSend } from "react-icons/io5";
 
@@ -8,6 +8,8 @@ interface ContactData {
   message: string;
 }
 
+const emailRegex = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
 const ContactForm = () => {
   const { t } = useTranslation();
   const [data, setData] = useState<ContactData>({
@@ -15,55 +17,106 @@ const ContactForm = () => {
     email: "",
     message: "",
   });
+  const [errors, setErrors] = useState<Partial<ContactData>>({});
+
+  const validate = (data: ContactData) => {
+    let errors: Partial<ContactData> = {};
+
+    if (!data.name.trim()) {
+      errors.name = "El nombre es requerido";
+    } else if (data.name.trim().length > 50) {
+      errors.name = "No puede tener mas de 50 caracteres";
+    }
+
+    if (!data.email.trim()) {
+      errors.email = "El email es requerido";
+    } else if (!emailRegex.test(data.email.trim())) {
+      errors.email = "No es un email valido";
+    }
+
+    if (!data.message.trim()) {
+      errors.message = "El mensaje es requerido";
+    } else if (data.message.trim().length > 1500) {
+      errors.message = "No puede tener mas de 1500 caracteres";
+    }
+
+    return errors;
+  };
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const { name, value } = e.target;
 
-    setData({
+    const newData = {
       ...data,
       [name]: value,
-    });
+    };
+
+    setErrors(validate(newData));
+    setData(newData);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const submitErrors = validate(data);
+    setErrors(submitErrors);
+
+    if (!Object.keys(submitErrors).length) {
+      alert(JSON.stringify(data));
+    }
   };
 
   return (
-    <form className="mx-auto my-10 grid max-w-[680px] gap-4 text-inherit">
+    <form
+      className="mx-auto my-10 grid max-w-[680px] gap-4 text-inherit"
+      onSubmit={handleSubmit}
+    >
       <div className="grid gap-4 sm:grid-cols-2">
         <div>
           <input
-            className="w-full rounded-sm border-b-4 border-b-icon/40 p-2 placeholder:capitalize placeholder:text-secondary-800/80 focus:border-b-icon focus:outline-none dark:border-b-secondary-500/40 dark:bg-primary-100 dark:text-primary-800 dark:placeholder:text-primary-800/70 dark:focus:border-b-secondary-500"
+            className={`${errors.name ? "border-b-red-500" : "border-b-icon/40 focus:border-b-icon dark:border-b-secondary-500/40 dark:focus:border-b-secondary-500"} w-full rounded-sm border-b-4 p-2 placeholder:capitalize placeholder:text-secondary-800/80 focus:outline-none dark:bg-primary-100 dark:text-primary-800 dark:placeholder:text-primary-800/70`}
             type="text"
             name="name"
             placeholder={t("name")}
             value={data.name}
             onChange={handleChange}
           />
+          {errors.name && (
+            <span className="text-sm text-red-500">{errors.name}</span>
+          )}
         </div>
         <div>
           <input
-            className="w-full rounded-sm border-b-4 border-b-icon/40 p-2 placeholder:capitalize placeholder:text-secondary-800/80 focus:border-b-icon focus:outline-none dark:border-b-secondary-500/40 dark:bg-primary-100 dark:text-primary-800 dark:placeholder:text-primary-800/70 dark:focus:border-b-secondary-500"
+            className={`${errors.email ? "border-b-red-500" : "border-b-icon/40 focus:border-b-icon dark:border-b-secondary-500/40 dark:focus:border-b-secondary-500"} w-full rounded-sm border-b-4 p-2 placeholder:capitalize placeholder:text-secondary-800/80 focus:outline-none dark:bg-primary-100 dark:text-primary-800 dark:placeholder:text-primary-800/70`}
             type="text"
             name="email"
             placeholder={t("email")}
             value={data.email}
             onChange={handleChange}
           />
+          {errors.email && (
+            <span className="text-sm text-red-500">{errors.email}</span>
+          )}
         </div>
       </div>
       <div>
         <textarea
-          className="w-full rounded-sm border-b-4 border-b-icon/40 p-2 placeholder:capitalize placeholder:text-secondary-800/80 focus:border-b-icon focus:outline-none dark:border-b-secondary-500/40 dark:bg-primary-100 dark:text-primary-800 dark:placeholder:text-primary-800/70 dark:focus:border-b-secondary-500"
+          className={`${errors.message ? "border-b-red-500" : "border-b-icon/40 focus:border-b-icon dark:border-b-secondary-500/40 dark:focus:border-b-secondary-500"} w-full rounded-sm border-b-4 p-2 placeholder:capitalize placeholder:text-secondary-800/80 focus:outline-none dark:bg-primary-100 dark:text-primary-800 dark:placeholder:text-primary-800/70`}
           name="message"
           rows={8}
           placeholder={t("message")}
           value={data.message}
           onChange={handleChange}
         />
+        {errors.message && (
+          <span className="text-sm text-red-500">{errors.message}</span>
+        )}
       </div>
       <button
         type="submit"
-        className="flex max-w-fit items-center gap-2 rounded-md bg-icon px-4 py-2 capitalize text-primary-50 dark:bg-primary-800"
+        className="flex max-w-fit cursor-pointer items-center gap-2 rounded-md bg-icon px-4 py-2 capitalize text-primary-50 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-primary-800"
+        disabled={Object.keys(errors).length ? true : false}
       >
         {t("send")}
         <IoSend className="icon" />
