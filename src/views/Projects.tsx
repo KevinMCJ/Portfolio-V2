b64d6b7789a831bad437be57014410b491a880e4
Kevin Mamani <kevin72309@gmail.com>
2025-03-19 13:05:53 -0300

    feat: initial "projects" view message

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,5 +1,15 @@
+import { useTranslation } from "react-i18next";
+
 const Projects = () => {
-  return <div>There will be more projects here</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div className="center min-h-screen">
+      <h2 className="section-title font-semibold tracking-wide">
+        {t("content.v_projects_provisional")}
+      </h2>
+    </div>
+  );
 };
 
 export default Projects;
